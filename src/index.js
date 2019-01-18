0e622dc193d8b575f01c72025130fcaf949e83b9
arainay <arainay@gmail.com>
2019-01-18 17:03:53 +0800

    Added apollo-link and apollo-link-error

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { ApolloClient } from 'apollo-client';
+import { ApolloLink } from 'apollo-link';
 import { HttpLink } from 'apollo-link-http';
+import { onError } from 'apollo-link-error';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { ApolloProvider } from 'react-apollo';
 
@@ -11,10 +13,21 @@ const httpLink = new HttpLink({
   uri: 'PUT YOUR GRAPHQL SERVER URL HERE'
 });
 
+const errorLink = onError(({ graphQLErrors, networkErrors }) => {
+  if (graphQLErrors) {
+    // do something with GraphQL errors
+  }
+  if (networkErrors) {
+    // do something with network errors
+  }
+});
+
+const link = ApolloLink.from([errorLink, httpLink]);
+
 const cache = new InMemoryCache();
 
 const client = new ApolloClient({
-  link: httpLink,
+  link,
   cache
 });
 
